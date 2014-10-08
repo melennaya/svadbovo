6c6d93d9f863b274ede1c97855d0b63dc13e8fd0
melennaya <tatiana.melennaya@gmail.com>
2014-10-08 12:12:52 +0300

    внесла правки, доверстала все основные страницы( кроме всплывающих окон), вставила слайдеры

--- javascript/script.js
+++ javascript/script.js
@@ -25,26 +25,43 @@ $(document).ready(function() {
         $(".file-upload").find("div."+p+"").removeAttr('id').siblings().attr('id', 'hide');
     });
 
-  
+
 
     $('.toggle').click(function(){
         if ($('#filter').is(":visible")) {
-        $('#filter').slideUp("fast", function() {
-            $('.options-up').removeClass('options-up').addClass('options-down');
-            $('.options-down').text('Параметры подбора')
-          });
+            $('#filter').slideUp("fast", function() {
+                $('.options-up').removeClass('options-up').addClass('options-down');
+                $('.options-down').text('Параметры подбора')
+            });
         }else{
-         $('#filter').slideDown("fast", function() {
+           $('#filter').slideDown("fast", function() {
             $('.options-down').removeClass('options-down').addClass('options-up');
             $('.options-up').text('Скрыть параметры')
-          });
-        }
-        return false;
+        });
+       }
+       return false;
+   });
+//carousels catalog_spisok_brand
+
+    for (var i = 1; i <= 6; i++) {
+        $('#carousel'+i).carouFredSel({
+        items               : 4,
+        direction           : "left",
+        //prev: '#prev1',
+        next: '#next'+i,
+        auto: false,
+        height: 211,
+        width: 675,
+        scroll : {
+            items           : 1,
+            easing          : false,
+            duration        : 1000,                         
+            pauseOnHover    : true
+        }                   
     });
-     
+    };
 
-    $("#carousel").clickCarousel({ margin: 18 });
-
+//end carousels
     $(".sliderkit").sliderkit({
         shownavitems: 8,
         circular: true,
@@ -60,12 +77,12 @@ $(document).ready(function() {
         $(".mask, .modal-view-photo").fadeIn('fast');
         $(".sliderkit-nav-clip, .sliderkit-nav-clip ul, .sliderkit-nav-clip ul li").removeClass("sliderkit-selected");
         $(".modal-view-photo .sliderkit-panels .sliderkit-panel")
-            .eq(counter)
-            .addClass("sliderkit-panel-active")
-            .css("display", "block");
+        .eq(counter)
+        .addClass("sliderkit-panel-active")
+        .css("display", "block");
         $(".sliderkit-nav .sliderkit-nav-clip ul li")
-            .eq(counter)
-            .addClass("sliderkit-selected");
+        .eq(counter)
+        .addClass("sliderkit-selected");
         var img = $('.sliderkit-panel img').eq(counter);
         var width  = $(img).width();
         var height = $(img).height();
@@ -89,26 +106,26 @@ $(document).ready(function() {
         $(".modal-comments .comments").css("height", modHeight-100);
     });
 
-    $(".sliderkit-go-prev, .sliderkit-go-next, .sliderkit-nav-clip ul li").click(function() {
-        var img = 0;
-        var img = $('.sliderkit-panel-active img');
-        var width  = $(img).width();
-        var height = $(img).height
-        var modHeight = $(".modal-view").height();
-        $(".modal-view, .sliderkit, .sliderkit-panel-active, .sliderkit-nav-clip").css("width", width);
-        $(".modal-view-photo").css("width", width + 415);
-        $(".sliderkit, .sliderkit-nav").css("height", height + 81);
-        $(".modal-comments").css("height", modHeight);
-        if (width < 595) {
-            $(".modal-view-photo .meta .fright, .modal-view-photo .meta .fleft").css("float", "none");
-            $(".modal-view-photo .meta .fright").css("width", "100%");
-        } else {
-            $(".modal-view-photo .meta .fleft").css("float", "left");
-            $(".modal-view-photo .meta .fright").css({"float" : "right", "width" : "68%"});
-        }
-        var modHeight = $(".modal-view").height();
-        $(".modal-comments").css("height", modHeight + 40);
-        $(".modal-comments .comments").css("height", modHeight-100);
-    });
-    
+$(".sliderkit-go-prev, .sliderkit-go-next, .sliderkit-nav-clip ul li").click(function() {
+    var img = 0;
+    var img = $('.sliderkit-panel-active img');
+    var width  = $(img).width();
+    var height = $(img).height
+    var modHeight = $(".modal-view").height();
+    $(".modal-view, .sliderkit, .sliderkit-panel-active, .sliderkit-nav-clip").css("width", width);
+    $(".modal-view-photo").css("width", width + 415);
+    $(".sliderkit, .sliderkit-nav").css("height", height + 81);
+    $(".modal-comments").css("height", modHeight);
+    if (width < 595) {
+        $(".modal-view-photo .meta .fright, .modal-view-photo .meta .fleft").css("float", "none");
+        $(".modal-view-photo .meta .fright").css("width", "100%");
+    } else {
+        $(".modal-view-photo .meta .fleft").css("float", "left");
+        $(".modal-view-photo .meta .fright").css({"float" : "right", "width" : "68%"});
+    }
+    var modHeight = $(".modal-view").height();
+    $(".modal-comments").css("height", modHeight + 40);
+    $(".modal-comments .comments").css("height", modHeight-100);
+});
+
 });
